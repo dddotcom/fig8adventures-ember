4697fbe3ceabddf4b888c374da3f7f4b761bf36c
dddotcom <dddotcom@gmail.com>
2017-01-13 14:00:31 -0800

    added ember-modal-dialog code to navbar-header component hbs and js files. modal now works on every page

--- app/controllers/philosophy.js
+++ app/controllers/philosophy.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isShowingModal: false,
-  actions: {
-    toggleModal: function() {
-      this.toggleProperty('isShowingModal');
-    }
-  }
+  
 });
